f045999051886ee9026e6c0fb30ac5a690bf9df5
Owen Caldwell <caldwellom@gmail.com>
2024-11-12 18:17:17 -0500

    added comments to imageinjector

--- imageinjector.js
+++ imageinjector.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Get the image parent element
     let imageParent = document.querySelector("grid-container");
+    // Some of my pages have the image parent element as image-wrapper, so I have to include this conditional, but it is not necessary.
     if (!imageParent) {
         imageParent = document.querySelector("image-wrapper");
     }
@@ -9,12 +11,13 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let i = 1; i <= imageCount; i++) {
         const imgSlide = document.createElement("img-slide");
         const img = document.createElement("img");
-
+        // Setting the srcset attribute for responsive images
         img.setAttribute("srcset", `
             dump/${baseName}_${i}_1500.webp 1500w,
             dump/${baseName}_${i}_1080.webp 1080w,
             dump/${baseName}_${i}_700.webp 700w
         `);
+        // Setting the src attribute for fallback
         img.setAttribute("src", `dump/${baseName}_${i}_1500.webp`);
         img.setAttribute("alt", "");
 
